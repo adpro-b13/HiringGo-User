f1aedfe79a4abcd2a124fd4f7a1b0dcf35cdcdfd
fassabilf <fassabilf@gmail.com>
2025-04-09 09:42:41 +0700

    chore: initialize project for user-service

---
+++ src/main/java/id/ac/ui/cs/advprog/b13/hiringgo/user/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.b13.hiringgo.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
